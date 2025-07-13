cf8de86ba0101ee6dcd62f61be12406fbc437cea
Arslan <91959072+rasulow@users.noreply.github.com>
2025-07-13 20:58:29 +0500

    new car store

--- owadan/settings.py
+++ owadan/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-r^e6oqt!(u-%5)w-!kfo&rz&u)dmlm7@pslm46s18=b+8faqb9
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,3 +143,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
